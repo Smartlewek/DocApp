eb469a18bad3896d1d11322a4e75f4213ee6ee73
Smartlewek <140055937+Smartlewek@users.noreply.github.com>
2025-03-06 14:54:54 +0100

    Update admin_panel.php

--- admin_panel.php
+++ admin_panel.php
@@ -257,6 +257,7 @@ $result = $conn->query($sql);
         <button id="addUserBtn" class="btn btn-success" onclick="toggleAddUserForm()">Dodaj użytkownika</button>
         <a href="testowa.php" class="btn btn-secondary">Dokumentacja</a>
         <button id="viewLogsBtn" class="btn btn-secondary" onclick="fetchLogs()">Pokaż logi</button>
+	 <button class="btn btn-danger" onclick="clearDevicesTable()">Wyczyść tabelę Devices</button>
 	 <div class="dropdown">
                 <button class="btn btn-primary dropdown-toggle" type="button" id="exportDropdown" data-bs-toggle="dropdown" aria-expanded="false">
                     Eksportuj bazy danych
@@ -375,6 +376,21 @@ function closeMessageModal() {
     const messageModal = bootstrap.Modal.getInstance(messageModalElement);
     if (messageModal) {
         messageModal.hide();
+    }
+}
+function clearDevicesTable() {
+    if (confirm("Czy na pewno chcesz usunąć wszystkie dane z tabeli Devices?")) {
+        fetch('clear_devices.php', {
+            method: 'POST'
+        })
+        .then(response => response.json())
+        .then(data => {
+            let message = data.success ? data.success : data.error;
+            showMessage(message);
+        })
+        .catch(error => {
+            showMessage('Wystąpił błąd podczas czyszczenia tabeli');
+        });
     }
 }
 </script>
